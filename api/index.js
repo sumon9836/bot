d07c109dea509d0b182fa4f74b7a69dff5a082af
𝘴น𝚖𝔞ꪦ_𝗿ǿⲩ <panelbot10@gmail.com>
2025-09-11 19:02:44 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 
 const PORT = process.env.PORT || 5000;
-const API_BASE_URL = 'http://crossover.proxy.rlwy.net:57524';
+const API_BASE_URL = 'http://ballast.proxy.rlwy.net:23161';
 
 // MIME types for static files
 const mimeTypes = {
